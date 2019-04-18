a650d1dd115d5b9a3d29e6fe2be0afcfdf5268f8
Eric Groom <egroom@mail.sfsu.edu>
2019-04-18 09:28:34 -0700

    project init

---
+++ Vocabuleasy/Vocabuleasy/RedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RedViewController.swift
+//  Vocabuleasy
+//
+//  Created by Eric Groom on 4/18/19.
+//  Copyright © 2019 Eric Groom. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor(hue: 0.5, saturation: 1, brightness: 1, alpha: 1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Vocabuleasy/VocabuleasyTests/VocabuleasyTests.swift
@@ -0,0 +1,34 @@
+//
+//  VocabuleasyTests.swift
+//  VocabuleasyTests
+//
+//  Created by Eric Groom on 4/18/19.
+//  Copyright © 2019 Eric Groom. All rights reserved.
+//
+
+import XCTest
+@testable import Vocabuleasy
+
+class VocabuleasyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
