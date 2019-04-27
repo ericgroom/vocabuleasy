5fdbb297af650d6cf67573072946c381930c8a74
Eric Groom <egroom@mail.sfsu.edu>
2019-04-27 10:36:46 -0700

    customized nav bar styling

--- Vocabuleasy/Vocabuleasy/AppDelegate.swift
+++ Vocabuleasy/Vocabuleasy/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let rootVC = RootViewController()
-        window?.rootViewController = rootVC
+        let navigationController = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
         Hero.shared.containerColor = .clear
