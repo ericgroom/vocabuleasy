f71e5f27b085307e72c37077aa33d1226019c40a
Eric Groom <egroom@mail.sfsu.edu>
2019-05-15 19:20:25 -0700

    remove deprecated reqparser

--- VocabuleasyApi/app/resources/sentences.py
+++ VocabuleasyApi/app/resources/sentences.py
@@ -1,19 +1,16 @@
 from random import choices
-from flask_restful import Resource, reqparse, abort
+from flask import request
+from flask_restful import Resource, abort
 from app.services import get_sentences
-
-parser = reqparse.RequestParser()
-parser.add_argument(
-    "lang", help="iso639-3 code for language you want to search for")
-parser.add_argument("word", help="word to search for")
 
 
 class SentenceResource(Resource):
-    def get(self):
-        args = parser.parse_args(strict=True)
-        word, lang = args["word"], args["lang"]
+    def post(self):
+        body = request.get_json()
+        word, lang = body["word"], body["lang"]
 
         if not word or not lang:
             abort(400, message="Missing argument word or lang")
+
         sentences = [entry["sentence"] for entry in get_sentences(lang, word)]
         return {'sentences': sentences}
